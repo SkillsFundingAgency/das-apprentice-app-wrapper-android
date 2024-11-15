99d93ffa36079da882f2389d29e6371b95d7ccea
abroodie <abroodie@gmail.com>
2024-11-15 11:28:52 +0000

    initial commit of package build

---
+++ src/app/src/main/java/uk/gov/education/myapprenticeship/DelegationService.java
@@ -0,0 +1,14 @@
+package uk.gov.education.myapprenticeship;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
